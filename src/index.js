8c7d295ccf8566fe97b22307c73319cfbf7868db
Yash Gandhi <gyash557@gmail.com>
2023-10-18 20:35:23 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={window.location.pathname || ''}>
-      <Route path='/' element={<App/>} />
-   </BrowserRouter>
+   <HashRouter base="/">
+   <App />
+</HashRouter>
 );
 
 
