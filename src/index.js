3dde80cc8a0e7bb886de1a9ffe0141412bb8f4ca
Yash Gandhi <gyash557@gmail.com>
2023-10-19 18:10:47 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+render(
    <HashRouter base="/">
    <App />
 </HashRouter>
